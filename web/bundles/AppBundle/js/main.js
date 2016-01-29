e6e337528a7608f27e1d678631a62afa0487cb07
Snafu <steph63b@hotmail.fr>
2016-01-29 09:31:09 +0100

    animate & slide Up Header

--- web/bundles/AppBundle/js/main.js
+++ web/bundles/AppBundle/js/main.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-      $('.parallax').parallax();
-    });
+	$('.parallax').parallax();
+});
+
+$(window).load(function()
+{
+	if(document.body.scrollTop!==0 || document.documentElement.scrollTop!==0){
+		$(".underHeader").slideUp('fast');
+	}
+});
+
+//Remonte le underHeader quand scroll
+window.onscroll = function(e){
+	if(window.innerWidth > 768)
+	{
+		if (document.body.scrollTop!==0 || document.documentElement.scrollTop!==0){
+			$(".underHeader").slideUp('fast');
+		}
+		else{
+			$(".underHeader").slideDown();
+		}
+	}
+	else{
+		$(".underHeader").css("position","absolute");
+		$(".underHeader").css("display","block");
+	}
+}
