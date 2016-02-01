7ac72c8503444831b1c592a07e02e079685d9a61
Juliette Rivière <juliette@riviere.im>
2016-02-01 10:17:26 +0100

    toastr on login

--- web/bundles/AppBundle/js/main.js
+++ web/bundles/AppBundle/js/main.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
 	$('.parallax').parallax();
+
+
+	    // toastr options
+
+/*    toastr.options = {
+    	"closeButton": true,
+    	"debug": false,
+    	"newestOnTop": false,
+    	"progressBar": false,
+    	"positionClass": "toast-top-center",
+    	"preventDuplicates": false,
+    	"onclick": null,
+    	"showDuration": "1000",
+    	"hideDuration": "1000",
+    	"timeOut": "5000",
+    	"extendedTimeOut": "1000",
+    	"showEasing": "swing",
+    	"hideEasing": "linear",
+    	"showMethod": "fadeIn",
+    	"hideMethod": "fadeOut"
+    }
+*/
 });
 
 $(window).load(function()
