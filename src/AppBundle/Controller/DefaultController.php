a17c826f060e679bbb48f09ce501415d2477500d
Snafu <steph63b@hotmail.fr>
2016-02-10 08:48:36 +0100

    hoverable

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -558,6 +558,10 @@ class DefaultController extends Controller
                 break;
             }
 
+            if($data['nbPas'] == null || $data['nbKm'] == null){
+                $data['error'] = true;
+            }
+
             return $this->render('AppBundle:Ajax:ajax_add_defis.html.twig', array(
                 "data" => $data
                 ));
